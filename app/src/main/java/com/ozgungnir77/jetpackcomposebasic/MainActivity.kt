bf61d1bb02814aaebc0446f068b7bc7f2fc6e47b
ozgungnir77 <ozgungnir77@icloud.com>
2021-12-13 15:37:00 +0800

    Composable functions

--- app/src/main/java/com/ozgungnir77/jetpackcomposebasic/MainActivity.kt
+++ app/src/main/java/com/ozgungnir77/jetpackcomposebasic/MainActivity.kt
@@ -14,25 +14,20 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            JetpackComposeBasicTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
-            }
+            MessageCard(name = "Android")
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun MessageCard(name: String) {
+    Text(text = "Hello $name")
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     JetpackComposeBasicTheme {
-        Greeting("Android")
+        MessageCard(name = "Android")
     }
 }
